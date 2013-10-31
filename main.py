ad3778a93bb7ff81af868c437caa18467869a1a9
Thiemo Gries <thiemo.gries@gmail.com>
2013-10-31 17:29:41 +0100

    clean up...again

--- main.py
+++ main.py
@@ -17,36 +17,27 @@ import imgtools
 
 class Kinector(object):
     """ Does awesome stuff with the Kinect. """
-    def __init__(self, kinect, filters=[], dummymode=False, buffersize=3, showoverlay=False, record=False, canny=False, hough=False):
+    def __init__(self, kinect, args=[], buffersize=3):
         self.running = False
+        self.record = 'record' in args
+
         self.kinect = kinect
-        self.smoothBuffer = imgtools.SmoothBuffer(buffersize)
-        self.dummymode = dummymode
-        self.showoverlay = showoverlay
-        self.threshold = np.empty(shape=(480, 640, 3)).fill(50)
-        self.balldetector = imgtools.BallDetector([180, 30, 30], threshold=100)
-        self.record = record
-        self.canny = canny
-        self.hough = hough
+
+        # TODO: implement useful buffer
+        # self.smoothBuffer = imgtools.SmoothBuffer(buffersize)
 
 
+        # init filters
         self.filters = []
-
-        if 'swapbackground' in filters:
+        if 'swapbackground' in args:
             self.filters.append(BackgroundFilter('bg.jpg'))
-
-        if 'disco' in filters:
+        if 'disco' in args:
             self.filters.append(DiscoFilter())
-
-        if 'canny' in filters:
+        if 'canny' in args:
             self.filters.append(CannyFilter())
-
-        if 'detectball' in filters:
+        if 'detectball' in args:
             self.filters.append(RectsFilter())
-
-        self.filters.append(NoFilter())
-
-        if 'overlay' in filters:
+        if 'overlay' in args:
             self.filters.append(OverlayFilter())
 
     def loop(self):
@@ -70,9 +61,6 @@ class Kinector(object):
         # reduce depth from 2048 to 256 values
         depth = depth / 8
 
-        args = {}
-        args['color'] = rgb[240, 320]
-
         for filter in self.filters:
             rgb, depth = filter.filter(rgb, depth, args)
 
@@ -85,10 +73,10 @@ class Kinector(object):
 if __name__ == '__main__':
     import sys
 
-    filters = []
+    args = []
     for argv in sys.argv:
         if argv.startswith('--'):
-            filters.append(argv[2:])
+            args.append(argv[2:])
 
     dummymode = "--dummymode" in sys.argv or "-d" in sys.argv
     if dummymode:
@@ -103,5 +91,5 @@ if __name__ == '__main__':
             kinect = KinectDummy()
             dummymode = True;
 
-    Kinector(kinect=kinect, filters=filters, dummymode=dummymode, record=False, canny=False, hough=False).loop()
+    Kinector(kinect=kinect, args=args).loop()
 
