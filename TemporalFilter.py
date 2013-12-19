39bde6ba27ba69532da74ef09c4c0ba6a8faf2f9
Thiemo Gries <thiemo.gries@gmail.com>
2013-12-19 14:32:59 +0100

    boxen

--- TemporalFilter.py
+++ TemporalFilter.py
@@ -1,59 +1,50 @@
 import numpy as np
 import vigra as vi
+import cv
 from DelayedBuffer import DelayedBuffer
 
 class TemporalFilter(object):
 
-    def __init__(self):
+    def __init__(self, minSize=100, maxRegions=50):
         self.buffer = DelayedBuffer(buffersize=5)
+        self.minSize = minSize
+        self.maxRegions = maxRegions
 
     def filter(self, rgb, depth, args = {}):
-        # d1 = self.buffer.get()
 
         self.buffer.add(depth)
-
-        # d2 = self.buffer.get()
 
         previous = self.buffer.get()
 
         if previous is not None:
-            vimg = vi.Image(previous < depth, dtype=np.uint8)
-            vimg = vi.filters.discErosion(vimg, 3)
-            vimg = vi.filters.discDilation(vimg, 1)
+            vi_img = vi.Image(previous > depth, dtype=np.uint8)
+            vi_img = vi.filters.discErosion(vi_img, 3)
+            vi_img = vi.filters.discDilation(vi_img, 1)
+            vi_img = vi_img.astype(np.float32)
 
-            vi_label_img = vi.analysis.labelImageWithBackground(vimg)
-            # vi.imshow(vi_label_img)
-            # vi.analysis.supportedRegionFeatures(diff, vi_label_img)
-            features = vi.analysis.extractRegionFeatures(diff, vi_label_img)
-            region_count = features.maxRegionLabel()+1
-            print region_count
+            vi_label_img = vi.analysis.labelImageWithBackground(vi_img)
 
-            # depth[diff == 0] = 0
+            vi.analysis.supportedRegionFeatures(vi_img, vi_label_img)
 
-            d = np.array(vi_label_img, dtype=np.uint16)
+            features = vi.analysis.extractRegionFeatures(vi_img, vi_label_img)
 
-            # d = d * 4095
-            # d[diff == 0] = 0
-            # d[d >= 4000] = 0
+            regions = features.maxRegionLabel() + 1
+            boxes = []
+            if regions <= self.maxRegions:
+                for i in range(1, features.maxRegionLabel() + 1):
+                    if features['Sum'][i] >= self.minSize:
+                        boxes.append((features['Coord<Minimum>'][i],features['Coord<Maximum>'][i]))
+
+            d = np.array(depth, dtype=np.uint16)
+
+            rgb_cv = cv.fromarray(np.array(rgb[:,:,::-1]))
+            for box in boxes:
+                [y, x] = box[0]
+                [y2, x2] = box[1]
+                cv.Rectangle(rgb_cv, (x, y), (x2, y2), cv.CV_RGB(0, 255,0), 2)
+            rgb = np.copy(rgb_cv)[:,:,::-1]
 
         else:
             d = depth
-
-        # if d1 is not None:
-            # diff = (d1 - d2) + 2048
-        # else:
-            # diff = d2
-
-        # print diff[300:305,300:305]
-
-
-        # trash = (diff < 2048 + 300)
-
-        # depth[(diff < 2048)] = 4095
-
-        # trash = diff > 0
-
-        # depth[trash] = 4095
-
 
         return rgb, d
