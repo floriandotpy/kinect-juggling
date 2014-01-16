bb00bb290c87c7b8c69ad9560072c9bc40193f44
Rolf <rolf.boomgaarden@googlemail.com>
2014-01-16 15:41:01 +0100

    merged TemporalFilter

--- TemporalFilter.py
+++ TemporalFilter.py
@@ -7,9 +7,8 @@ except:
 from DelayedBuffer import DelayedBuffer
 
 class TemporalFilter(object):
-
     def __init__(self, minSize=100, maxRegions=50, erosion=True):
-        self.buffer = DelayedBuffer(buffersize=5)
+        self.buffer = DelayedBuffer(buffersize=10)
         self.minSize = minSize
         self.maxRegions = maxRegions
         self.erosion = erosion
